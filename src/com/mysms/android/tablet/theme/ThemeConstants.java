f80656fcbc357e9dbd220c6c2898c6a88c57a2a1
mmarc <marc.mikolits@ut11.net>
2013-01-23 10:12:06 +0100

    ~ initial commit

---
+++ src/com/mysms/android/tablet/theme/ThemeConstants.java
@@ -0,0 +1,7 @@
+package com.mysms.android.tablet.theme;
+
+public class ThemeConstants {
+
+	public static final String INTENT_ACTION_THEME = "com.mysms.android.tablet.intent.action.THEME";
+	public static final String INTENT_EXTRA_THEME_ACTIVATE = "com.mysms.android.tablet.intent.extra.THEME_ACTIVATE";
+}
